fa2d5f8e8cf63978e202c69593ce3df67e2e10ee
Craig Baumgarten <craig@Craigs-MacBook-Air.local>
2024-07-07 14:38:03 -0500

    Adding boilerplate dir and file.

---
+++ src/app/main.py
@@ -0,0 +1,21 @@
+import time
+
+
+class HornGoHonk:
+    def __init__(self, location, t, coordinates):
+        self.location = location
+        self.t = t
+        self.coordinates = coordinates
+
+    def horn_count(self):
+        pass
+
+    def horn_estimate(self):
+        pass
+
+    def horn_frequency(self):
+        pass
+
+    def horn_history(self):
+        pass
+
