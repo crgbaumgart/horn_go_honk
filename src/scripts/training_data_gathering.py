2e3e528af7f5fdad00e10a9ed5213ddd27951741
thecrazybishop <donterry91@gmail.com>
2024-07-28 18:39:41 -0500

    updated escape conditions for recording loop to use a count of recordings instead of a timer. Also updated the file path to indicate the number of the recording instead of the time stamp of the recording

--- src/scripts/training_data_gathering.py
+++ src/scripts/training_data_gathering.py
@@ -94,31 +94,34 @@ def main():
     params::
     duration: length of audio recording in seconds
     timestamp: current time stamp at the time of execution. It is appened to the end of the file name to
-        version the files to avoid confusion
+        version the files to avoid confusio n
     desktop_directory: directory for storing the audio files
     filename: the file name for the audio file
     """
-    recording_duration = 180
+    recording_duration = 600
+    desired_number_of_recordings = 10
     recording_start_time = time.time()
     list_audio_devices()
+    start_counter = 0
     while True:
+        true_count = start_counter + 1
         current_time = time.time()
         elapsed_time = current_time - recording_start_time
         print(f"Function running for {elapsed_time} seconds")
 
-        if elapsed_time > recording_duration:
-            print("Time Limit Excceeded")
+        if true_count > desired_number_of_recordings:
+            print("Recording Limit Reached")
             break
         device_index = 1  # Replace with the actual device index from the list
         duration = 30
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        #  timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         #  while doing data collection, make sure to change the file path based on if the train is present,
         #  and also change the file name. File name should end in "NT" for no train and "TP" for a train
         #  being present.
         desktop_directory = os.path.join(os.path.expanduser("~"), "Documents", "horn_go_honk_testing",
                                          "Audio_training", "No Train")
         create_directory_if_not_exists(desktop_directory)
-        filename = f"{desktop_directory}/audio_recording_{timestamp}_NT.wav"
+        filename = f"{desktop_directory}/audio_recording_{true_count}_NT.wav"
         #  call recording function, passes file_name and duration back to the record_audio function
         record_audio(filename, duration)
 
